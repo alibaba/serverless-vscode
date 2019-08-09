9554d859300c2ee9e15127f01b7f5960e7a6f335
William <1021805382@qq.com>
2019-08-09 14:19:41 +0800

    feat(Template Jump): 跳转到服务或函数的 yaml 定义时，提供色块显示 (#54)
    re #46

--- src/utils/document.ts
+++ src/utils/document.ts
@@ -110,3 +110,78 @@ function resolveOffsetsOfLineStart(textDoc: string): number[] {
   }
   return lineOffsets;
 }
+
+export function findBlockEndLine(document: vscode.TextDocument, position: vscode.Position): number {
+  let lineNumber = position.line;
+  let lineTxt = document.lineAt(position.line).text;
+  const spaceCnt = countLeadingSpace(lineTxt);
+  let cnt = spaceCnt;
+  let isStart = true;
+  while(cnt > spaceCnt || isStart) {
+    isStart = false;
+    if (++lineNumber >= document.lineCount) {
+      break;
+    }
+    lineTxt = document.lineAt(lineNumber).text;
+    cnt = countLeadingSpace(lineTxt);
+  }
+  return lineNumber - 1;
+}
+
+export function countLeadingSpace(str: string) {
+  let preSpaceCnt = 0;
+  for (const ch of str) {
+    if (ch === ' ') {
+      preSpaceCnt++;
+    } else {
+      break;
+    }
+  }
+  return preSpaceCnt;
+}
+
+export function createDecorationTypes(
+  rgb: { r: number, g: number, b: number},
+  opacityStart: number,
+  opacityEnd: number,
+  step: number
+) {
+  const result: vscode.TextEditorDecorationType[] = [];
+  if (opacityStart > opacityEnd) {
+    let tmp = opacityEnd;
+    opacityEnd = opacityStart;
+    opacityStart = tmp;
+  }
+  for (let i = opacityStart; i <= opacityEnd; i += step) {
+    result.push(
+      vscode.window.createTextEditorDecorationType({
+        cursor: 'crosshair',
+        backgroundColor: `rgba(${rgb.r},${rgb.g},${rgb.b},${i})`
+      })
+    )
+  }
+  return result.reverse();
+}
+
+export function decorateEditor(
+  editor: vscode.TextEditor,
+  range: vscode.Range,
+  decorationTypes: vscode.TextEditorDecorationType[]
+) {
+  if (!editor || !decorationTypes || !decorationTypes.length) {
+    return;
+  }
+  editor.setDecorations(decorationTypes[0], [range]);
+  let decorationTypeIndex = 1;
+  const doDecorate = () => {
+    editor.setDecorations(decorationTypes[decorationTypeIndex - 1], []);
+    if (decorationTypeIndex < decorationTypes.length) {
+      editor.setDecorations(decorationTypes[decorationTypeIndex], [range]);
+      decorationTypeIndex++;
+      setTimeout(() => {
+        doDecorate();
+      }, 100)
+    }
+  }
+  doDecorate();
+}
