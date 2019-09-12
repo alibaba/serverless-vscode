22dbe101a307b651cc271f63b9643ab101ab106d
William <1021805382@qq.com>
2019-09-12 13:43:01 +0800

    feat(Fun Invoke): Replace remote invoke to fun invoke (#108)

--- src/commands/remoteInvokeFunction.ts
+++ src/commands/remoteInvokeFunction.ts
@@ -1,12 +1,11 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
-import * as fs from 'fs';
+import * as os from 'os';
 import { serverlessCommands } from '../utils/constants';
 import { isPathExists, createEventFile } from '../utils/file';
 import { recordPageView } from '../utils/visitor';
-import { FunctionComputeService } from '../services/FunctionComputeService';
-import { getFunctionComputeOutputChannel } from '../utils/channel';
 import { Resource, ResourceType, FunctionResource } from '../models/resource';
+import { FunService } from '../services/FunService';
 
 export function remoteInvokeFunction(context: vscode.ExtensionContext) {
   context.subscriptions.push(vscode.commands.registerCommand(serverlessCommands.REMOTE_INVOKE.id,
@@ -40,79 +39,10 @@ async function process(serviceName: string, functionName: string) {
       }
     }
   }
-
-  const event: string = cwd ? fs.readFileSync(eventFilePath, 'utf8') : '{}';
-  const functionComputeService = new FunctionComputeService();
-  const channel = getFunctionComputeOutputChannel();
-
-  channel.clear();
-  channel.show();
-  channel.appendLine(`invoke remote function: ${serviceName}/${functionName} ...`);
-
-  const task: Promise<any> = new Promise(resolve => {
-    const httpTriggerUrls: any[] = [];
-    functionComputeService.listTriggers(serviceName, functionName)
-      .then(triggers => {
-        (<any[]>triggers).forEach(trigger => {
-          if (trigger.triggerType === 'http') {
-            httpTriggerUrls.push(
-              `https://${functionComputeService.getAccountId()}.${functionComputeService.getRegion()}.` +
-              `fc.aliyuncs.com/2016-08-15/proxy/${serviceName}.${trigger.qualifier || 'LATEST'}/${functionName}/`
-            );
-          }
-        })
-        if (httpTriggerUrls.length > 0) {
-          const result = {
-            data: httpTriggerUrls.join('\n'),
-          }
-          channel.appendLine(
-            'This HTTP trigger can be accessed ' +
-            'through the folowing url by your favourite toolkit such as curl or postman.'
-          );
-          channel.appendLine('======================================================');
-          resolve(result);
-          return;
-        } else {
-          if (cwd) {
-            channel.appendLine('local event file path: '
-            + `${<string>vscode.workspace.getConfiguration().get('aliyun.fc.remoteSource.eventFile.path')}`);
-          }
-          channel.appendLine('======================================================');
-          functionComputeService.invokeFunction(serviceName, functionName, event)
-            .then(result => {
-              resolve(result);
-            });
-          return;
-        }
-      });
-  })
-
-  vscode.window.withProgress({
-    location: vscode.ProgressLocation.Notification,
-    title: 'invoke remote function ...',
-    cancellable: true,
-  }, (progress, token) => {
-    return task;
-  })
-
-  vscode.window.withProgress({
-    location: vscode.ProgressLocation.Window,
-    title: 'invoke remote function ...',
-  }, (progress, token) => {
-    return task;
-  })
-
-  task.then(result => {
-    const { data = '', headers = {} } = result;
-    channel.appendLine(data);
-    channel.appendLine('======================================================');
-    const logInfo = headers['x-fc-log-result'];
-    if (logInfo) {
-      try {
-        channel.appendLine(Buffer.from(logInfo, 'base64').toString('UTF8'));
-      } catch (ex) {
-        vscode.window.showErrorMessage(ex.message);
-      }
-    }
-  })
+  const funService = new FunService(cwd || os.homedir());
+  if (cwd) {
+    funService.remoteInvokeWithEventFilePath(serviceName, functionName, eventFilePath);
+  } else {
+    funService.remoteInvokeWithStdin(serviceName, functionName);
+  }
 }
